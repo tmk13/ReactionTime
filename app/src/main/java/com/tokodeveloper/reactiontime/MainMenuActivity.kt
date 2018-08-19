c8dcdace24e14dcd3d4cd999d0e9e8e6eda4fe19
tmk13 <tmk13@protonmail.ch>
2018-08-19 12:08:21 +0200

    init commit

---
+++ app/src/main/java/com/tokodeveloper/reactiontime/MainMenuActivity.kt
@@ -0,0 +1,12 @@
+package com.tokodeveloper.reactiontime
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainMenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_menu)
+    }
+}
