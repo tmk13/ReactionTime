f8ecfd199d0d414488058b8c1b27959205b9c56a
tmk13 <tmk13@protonmail.ch>
2018-08-21 13:04:53 +0200

    add game screen UI

--- app/src/main/java/com/tokodeveloper/reactiontime/viewmodels/GameViewModel.kt
+++ app/src/main/java/com/tokodeveloper/reactiontime/viewmodels/GameViewModel.kt
@@ -1,7 +1,11 @@
 package com.tokodeveloper.reactiontime.viewmodels
 
+import android.view.View
 import androidx.lifecycle.ViewModel;
 
 class GameViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+    fun userClickedButton(view: View?) {
+
+    }
 }
